786fd34a8dfe123b5b446ff098ad538b5e538751
manhgolabs <manhnguyen@golabs.vn>
2025-08-20 22:36:57 +0700

    fix host

--- docs/docs.go
+++ docs/docs.go
@@ -2946,7 +2946,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "gometro-backend-production.up.railway.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Go Metro API",

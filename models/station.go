bf7132cfce1b1e0caf5524589f78daa0e8b2e32d
manhgolabs <manhnguyen@golabs.vn>
2025-08-22 23:40:58 +0700

    update station

--- models/station.go
+++ models/station.go
@@ -1,18 +1,23 @@
 package models
 
 import (
-	"go-metro/config"
-	"time"
+  "time"
+
+  "go-metro/config"
 )
 
 type Station struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"not null" json:"name"`
-	IPAddress string    `json:"ip_address"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+  ID          uint      `gorm:"primaryKey" json:"id"`
+  Name        string    `gorm:"not null" json:"name"`
+  IPAddress   string    `json:"ip_address"`
+  Capacity    int       `json:"capacity"`
+  Address     string    `json:"address"`
+  Status      string    `json:"status"`
+  Description string    `json:"description"`
+  CreatedAt   time.Time `json:"created_at"`
+  UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func MigrateStation() {
-	config.DB.AutoMigrate(&Station{})
-} 
+  config.DB.AutoMigrate(&Station{})
+}
